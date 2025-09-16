9d6a0c813a58aa0ff921640e7d4d58e29f47d1f9
Thimoty Barbieri <thimoty@thimoty.it>
2025-09-17 01:21:37 +0200

    Upgrade to Spring Boot 3

--- src/test/java/tacos/HomePageBrowserTest.java
+++ src/test/java/tacos/HomePageBrowserTest.java
@@ -1,53 +1,52 @@
 package tacos;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
-public class HomePageBrowserTest {
+class HomePageBrowserTest {
   
   @LocalServerPort
   private int port;
-  private static HtmlUnitDriver browser;  
+  private static WebDriver browser;  
   
-  @BeforeClass
-  public static void setup() {
+  @BeforeAll
+  static void setup() {
     browser = new HtmlUnitDriver();
     
     browser.manage().timeouts()
-          .implicitlyWait(10, TimeUnit.SECONDS);
+          .implicitlyWait(Duration.ofSeconds(10));
   }
   
-  @AfterClass
-  public static void teardown() {
+  @AfterAll
+  static void teardown() {
     browser.quit();
   }
   
   @Test
-  public void testHomePage() {
+  void testHomePage() {
     String homePage = "http://localhost:" + port;
     browser.get(homePage);
     
     String titleText = browser.getTitle();
-    Assert.assertEquals("Taco Cloud", titleText);
+    Assertions.assertEquals("Taco Cloud", titleText);
     
-    String h1Text = browser.findElementByTagName("h1").getText();
-    Assert.assertEquals("Welcome to...", h1Text);
+    String h1Text = browser.findElement(By.tagName("h1")).getText();
+    Assertions.assertEquals("Welcome to...", h1Text);
     
-    String imgSrc = browser.findElementByTagName("img")
+    String imgSrc = browser.findElement(By.tagName("img"))
                                               .getAttribute("src");
-    Assert.assertEquals(homePage + "/images/TacoCloud.png", imgSrc);
+    Assertions.assertEquals(homePage + "/images/TacoCloud.png", imgSrc);
   }
   
   
--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,16 +1,13 @@
 package tacos;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)    // <1>
 @SpringBootTest                 // <2>
-public class TacoCloudApplicationTests {
+class TacoCloudApplicationTests {
 
   @Test                         // <3>
-  public void contextLoads() {
+  void contextLoads() {
   }
 
 }
